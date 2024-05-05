c59cc8d511c0a0c889e9998e4d7db891188168a2
Jazael Perez <131943183+perezjazaelr@users.noreply.github.com>
2024-05-06 04:52:20 +0800

    Add files via upload

---
+++ HTML introduction/INTRO-hello.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP LAMBEosaurus</title>
+    <link rel="Website icon" type="png" href="/PICTURES/Untitled-1.png">
+    <link rel="stylesheet" href="../CSS/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap" rel="stylesheet">    
+    <style>
+        .nav-bar{
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }   
+    </style>
+</head>
+<body>
+    <div class="main-container">
+        <div class="nav-bar">
+            <div class = "nav-cont">
+			<!--/*changes here*/ -->
+                <a href="#" class="toggle-button">
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                </a>
+                <h3>PHP Introduction</h3>
+            </div>
+            <img  onclick="Myland()" src="../PICTURES/Untitled-1.png" alt="">
+        </div>
+
+        <div class="sub-container">
+            <div class="sidebar">
+                <h3 onclick="Myhome()">Hello World</h3>
+                <h3 onclick="Mycomment()" >Comments</h3>
+                <h3 onclick="Myvariables()" >Variables</h3>
+                <h3 onclick="Myecho()" >Echo/Print</h3>
+                <h3 onclick="MyvarDump()" >var_dump</h3>
+                <h3 onclick="My$var()" >$ and $$ Variables</h3>
+                <h3 onclick="Mycons()" >Constants</h3>
+                <h3 onclick="MymagCons()" >Magic Constants</h3>
+                <h3 onclick="Mydata()" >Data Types</h3> 
+                <h3 onclick="MytypeCast()" >Type Casting</h3>
+                <h3 onclick="MytypeJug()" >Type Juggling</h3>
+                <h3 onclick="Mystrings()" >Strings</h3>
+                <h3 onclick="MyBoolean()" >Boolean</h3>
+                <h3 onclick="Myint()" >Integers</h3>
+                <h3 onclick="Myfiles()" >Files and I/O</h3>
+                <h3 onclick="Mymaths()" >Maths Function</h3>
+                <h3 onclick="Myhere()" >Heredoc & Nowdoc</h3>
+                <h3 onclick="Mycomp()" >Compound Types </h3> 
+            </div>
+
+            <div class="container-child">
+                <div class="header">
+                    <h3>PHP - Hello World</h3>
+                </div>
+                <div class="box-child">
+                    <div class="sub-header1">
+                        <h3>Description</h3>
+                    </div>
+                    <div class="sub-context">
+                        <p>Conventionally, learners write a "Hello World" program as their first program when learning a new language or a framework. The objective is to verify if the software to be used has been installed correctly and is working as expected. To run a Hello World program in PHP, you should have installed the Apache server along with PHP module on the operating system you are using.</p>
+                    </div>
+                </div>
+                <div class="box-child">
+                    <div class="sub-header1">
+                        <h3>Syntax</h3>
+                    </div>
+                    <div class="sub-context dark-syntax">
+                        <p><span class="opse-numbers">1.</span><span class="pink">&lt;php</span></p>
+                        <p><span class="numbers">2.</span><span class="yellow">echo</span> <span class="orange">"Hello World!"</span>;</p>
+                        <p><span class="opse-numbers">3.</span><span class="pink">?&gt;</span></p>
+                    </div>
+                </div>
+                <div class="box-child">
+                    <div class="sub-header1">
+                        <h3>Output</h3>
+                    </div>
+                    <div class="sub-context">
+                        <p>
+                           <?php
+                            echo "Hello World!";
+                        ?> 
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+	<?php
+        echo "Hello World!";
+    ?> 
+    <script src="/JS/script.js"></script>
+
+</body>
+</html>
---
+++ JS/script.js
@@ -0,0 +1,146 @@
+const toggleButon = document.getElementsByClassName('toggle-button')[0]
+			const navbarLinks = document.getElementsByClassName('sidebar')[0]
+
+			toggleButon.addEventListener('click', () => {
+				navbarLinks.classList.toggle('active')
+				navbarLinks.classList.add('link--transition')
+        })
+
+
+
+    function My_INTRODUCTION(){
+        location.href="PHP-INTRO.html"
+    }
+    function My_OPERATOR(){
+        location.href="OPERATOR.html"
+    }
+    function My_CONTROLSTATE(){
+        location.href="CONTROL.html"
+    }
+
+
+
+
+function Myland(){
+    location.href="landpage.html"
+}
+function Myhome(){
+    location.href="INTRO-hello.html"
+}
+function Mycomment(){
+    location.href="INTRO-comments.html"
+}
+function Myvariables(){
+    location.href="INTRO-Variables.html"
+}
+function Myecho(){
+    location.href="INTRO-echo.html"
+}
+function MyvarDump(){
+    location.href="INTRO-VarDump.html"
+}
+function My$var(){
+    location.href="INTRO-$and$$.html"
+}
+function Mycons(){
+    location.href="INTRO-Constant.html"
+}
+function MymagCons(){
+    location.href="INTRO-magicCons.html"
+}
+function Mydata(){
+    location.href="INTRO-DataTypes.html"
+}
+function MytypeCast(){
+    location.href="INTRO-typeCast.html"
+}
+function MytypeJug(){
+    location.href="INTRO-typeJug.html"
+}
+function Mystrings(){
+    location.href="INTRO-String.html"
+}
+function MyBoolean(){
+    location.href="INTRO-boolean.html"
+}
+function Myint(){
+    location.href="INTRO-int.html"
+}
+function Myfiles(){
+    location.href="INTRO-files.html"
+}
+function Mymaths(){
+    location.href="INTRO-math.html"
+}
+function Myhere(){
+    location.href="INTRO-here.html"
+}
+function Mycomp(){
+    location.href="INTRO-CompTypes.html"
+}
+
+
+
+function Myopera(){
+    location.href="OPER-operator.html"
+}
+function Myarit(){
+    location.href="OPER-arith.html"
+}
+function Mycompa(){
+    location.href="OPER-compOper.html"
+}
+function Mylogical(){
+    location.href="OPER-logOper.html"
+}
+function Myass(){
+    location.href="OPER-assOper.html"
+}
+function Mystring(){
+    location.href="OPER-stringOper.html"
+}
+function Myarr(){
+    location.href="OPER-arrayOper.html"
+}
+function Mycondi(){
+    location.href="OPER-condiOper.html"
+}
+function Myspread(){
+    location.href="OPER-spreadOper.html"
+}
+function Mynull(){
+    location.href="OPER-nullOper.html"
+}
+function Myspace(){
+    location.href="OPER-SpaceOper.html"
+}
+
+
+
+function Mydeci(){
+    location.href="CONT-Desci.html"
+}
+function Myifelse(){
+    location.href="CONT-ifelse.html"
+}
+function Myswitch(){
+    location.href="CONT-Switch.html"
+}
+function MyforLoop(){
+    location.href="CONT-forLoop.html"
+}
+function MyforeachLoop(){
+    location.href="CONT-foreachLoop.html"
+}
+function MywhileLoop(){
+    location.href="CONT-whileLoop.html"
+}
+function MydowhileLoop(){
+    location.href="CONT-dowhileLoop.html"
+}
+function Mybreak(){
+    location.href="CONT-breakStatement.html"
+}
+function MyConti(){
+    location.href="CONT-continueStatement.html"
+}
